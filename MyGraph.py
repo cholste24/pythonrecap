2aeac32b230769c22ee6a2ae7511b70d87b98173
Collin <collin_holste1@baylor.edu>
2024-01-24 14:41:22 -0600

    updated gitignore and print statement

--- MyGraph.py
+++ MyGraph.py
@@ -6,4 +6,11 @@ plt.plot(x, np.sin(x))
 
 plt.show()
 
-print("Hi there!")
+print("Hello World!")
+
+# Creating a Virtual Environment
+# 1. Create a VE - mac: python3 -m venv myvenv
+#                - pc: py -3 -m venv _____
+# 2. Activate the VE - mac: source myvenv/bin/activate
+#                    - pc: myvenv/scripts/activate
+# 3. Install 3rd party library/module: pip3 install _______
